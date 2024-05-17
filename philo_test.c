5ac532a67d22cfea64e4260644e1724dddb64de1
tomjoy75 <tomjoy75@gmail.com>
2024-05-17 15:42:12 +0200

    Les philosophes interagissent

--- philo_test.c
+++ philo_test.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo_test.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joyeux <joyeux@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tjoyeux <tjoyeux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 10:15:05 by tjoyeux           #+#    #+#             */
-/*   Updated: 2024/05/17 00:40:59 by joyeux           ###   ########.fr       */
+/*   Updated: 2024/05/17 15:38:10 by tjoyeux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,9 @@ typedef struct s_philo
 	pthread_mutex_t	l_fork;
 	pthread_mutex_t	*r_fork;
 	int				state;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int				time_to_die;
+//	int				time_to_eat;
+//	int				time_to_sleep;
+//	int				time_to_die;
 }				t_philo;
 
 typedef struct s_rules
@@ -46,45 +46,67 @@ typedef struct s_args
 	t_rules *rules;
 }				t_args;
 
-void	timestamp(t_philo philo)
+int	timestamp(t_philo philo, t_rules rules, int state)
 {
-	if (philo.state == 1)
-		printf("timestamp_in_ms %d has taken a fork", philo.id);
+	struct timeval	tv_act;
+	int				time_passed;
+
+	if (gettimeofday(&tv_act, NULL))
+		return (1);
+	time_passed = (((tv_act.tv_sec - rules.tv_beg.tv_sec) * 1000) + ((tv_act.tv_usec - rules.tv_beg.tv_usec) / 1000));
+	if (state == 1)
+		printf("%d\t%d has taken a fork\n", time_passed, philo.id);
+	if (state == 2)
+		printf("%d\t%d is eating\n", time_passed, philo.id);
+	if (state == 3)
+		printf("%d\t%d is sleeping\n", time_passed, philo.id);
+	if (state == 4)
+		printf("%d\t%d is thinking\n", time_passed, philo.id);
+	if (state == 5)
+		printf("%d\t%d died\n", time_passed, philo.id);
 }
 
 void	*eat(void *args)
 {
 	t_philo	*philo = ((t_args *)args)->philo;
 	t_rules	*rules = ((t_args *)args)->rules;
-	printf("OK Philo %d...It began at %ld\n", philo->id, rules->tv_beg.tv_sec);
-	sleep(2);
+	if (philo->id % 2 == 1 )
+		usleep(100);
 	while (1)
 	{
 		if (philo->id % 2 == 0)
 		{
 			pthread_mutex_lock(&(philo->l_fork));
+			timestamp(*philo, *rules, 1);
 			pthread_mutex_lock(philo->r_fork);
 		}
 		else
 		{
 			pthread_mutex_lock(philo->r_fork);
+			timestamp(*philo, *rules, 1);
 			pthread_mutex_lock(&(philo->l_fork));
 		}
-		philo->state == 1;
-		timestamp(*philo);
+/*		pthread_mutex_lock(philo->r_fork);
+		timestamp(*philo, *rules, 1);
+		pthread_mutex_lock(&(philo->l_fork));*/
+//		philo->state == 1;
+		timestamp(*philo, *rules, 2);
 //		printf("Philo's [%d] eating\n", philo->id);
-		usleep(philo->time_to_sleep * 1000);
+		usleep(rules->time_to_eat * 1000);
 		pthread_mutex_unlock(&(philo->l_fork));
 		pthread_mutex_unlock(philo->r_fork);
-		printf("Philo's [%d] sleeping\n", philo->id);
-		usleep(philo->time_to_sleep * 1000);
-		printf("Philo's [%d] thinking\n", philo->id);
+		timestamp(*philo, *rules, 3);
+//		printf("Philo's [%d] sleeping\n", philo->id);
+		usleep(rules->time_to_sleep * 1000);
+		timestamp(*philo, *rules, 4);
+		usleep(100);
+//		printf("Philo's [%d] thinking\n", philo->id);
 	}
 }
 
 int	main()
 {
-	int		nb_philo = 4;
+	int		nb_philo = 5;
 	t_philo	philos[nb_philo];
 	t_rules	rules;
 	t_args	*args;
@@ -93,9 +115,9 @@ int	main()
 	// Creer tableau de philo
 	// Remplir les rules
 	rules.nb_philo = nb_philo;
-	rules.time_to_die = 100;
-	rules.time_to_eat = 100;
-	rules.time_to_sleep = 100;
+	rules.time_to_die = 700;
+	rules.time_to_eat = 200;
+	rules.time_to_sleep = 200;
 	if (gettimeofday(&(rules.tv_beg), NULL))
 		return (1);
 	// Remplir ces caracteristiques
@@ -111,15 +133,16 @@ int	main()
 //			error = philos[i].tv_beg
 		i++;
 	}
+	args = malloc(sizeof(t_args) * nb_philo);
 	// Envoyer programme
 	i = 0;
 	while (i < nb_philo)
 	{
 		if (philos[i].id % 2 == 1 )
-			usleep(100);
-		args->philo = &philos[i];
-		args->rules = &rules;
-		pthread_create(&(philos[i].t_id), NULL, &eat, (void *)args);
+				usleep(100);
+		args[i].philo = &(philos[i]);
+		args[i].rules = &rules;
+		pthread_create(&(philos[i].t_id), NULL, &eat, (void *)&args[i]);
 		i++;
 	}
 	// thread_join
