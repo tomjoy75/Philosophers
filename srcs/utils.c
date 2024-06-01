30b72eb850d9a31846910fcdbdf16c682dcadd91
tomjoy75 <tomjoy75@gmail.com>
2024-06-01 17:00:15 +0200

    Changement de timestamp

--- srcs/utils.c
+++ srcs/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joyeux <joyeux@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tjoyeux <tjoyeux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 10:15:46 by tjoyeux           #+#    #+#             */
-/*   Updated: 2024/05/31 23:02:36 by joyeux           ###   ########.fr       */
+/*   Updated: 2024/06/01 16:54:56 by tjoyeux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ int	error_exit(const char *error)
 {
 	printf(RED"Error :\n\t"ITALIC"%s\n"RESET, error);
 	return (-1);
-} 
+}
 
-void	*error_exit_NULL(const char *error)
+void	*error_exit_null(const char *error)
 {
 	printf(RED"Error :\n\t"ITALIC"%s\n"RESET, error);
 	return (NULL);
@@ -37,45 +37,39 @@ void	clean_all(t_philo *philos, int n_mutex)
 	free (philos);
 }
 
+static char	*activity(int state)
+{
+	if (state == 1)
+		return ("has taken a fork");
+	else if (state == 2)
+		return ("is eating");
+	else if (state == 3)
+		return ("is sleeping");
+	else if (state == 4)
+		return ("is thinking");
+	else if (state == 5)
+		return ("died");
+	else if (state == 6)
+		return ("___check dying___");
+	return ("");
+}
+
 int	timestamp(t_philo *philo, t_rules *rules, int state)
 {
 	struct timeval	tv_act;
 	int				time_passed;
 
-//	if (!rules.nb_eating)
-//		return (1);
-//	usleep(100);
 	if (gettimeofday(&tv_act, NULL))
 		return (1);
 	time_passed = (((tv_act.tv_sec - rules->tv_beg.tv_sec) * 1000)
 			+ ((tv_act.tv_usec - rules->tv_beg.tv_usec) / 1000));
-//	printf("Attemting to lock print_mutex\n");
 	pthread_mutex_lock(&rules->global_mutex);
-//	printf("print_mutex locked\n");
 	if (!rules->write_off)
 	{
-		if (state == 1)
-			printf("%d\t%d has taken a fork\n", time_passed, philo->id);
-		else if (state == 2)
-			printf("%d\t%d is eating\n", time_passed, philo->id);
-		else if (state == 3)
-			printf("%d\t%d is sleeping\n", time_passed, philo->id);
-		else if (state == 4)
-			printf("%d\t%d is thinking\n", time_passed, philo->id);
-		else if (state == 5)
-	//		printf("\e[31m%d\t%d died\n\e[0m", time_passed, philo.id);
-		{
-			printf("%d\t%d died\n", time_passed, philo->id);
+		printf("%d\t%d %s\n", time_passed, philo->id, activity(state));
+		if (state == 5)
 			rules->write_off = 1;
-		}
-		else if (state == 6)
-			printf("%d\t%d ___check dying___\n", time_passed, philo->id);
 	}
-	
 	pthread_mutex_unlock(&rules->global_mutex);
-//	printf("Attemting to unlock print_mutex\n");
-//	pthread_mutex_unlock(&rules->global_mutex);
-//	printf("print_mutex unlocked\n");
 	return (0);
 }
-
